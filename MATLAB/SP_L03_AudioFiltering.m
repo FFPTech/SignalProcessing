cb47a2025b836cadd2b71a8e8fdd36c13b651c6c
Fit-For-Purpose Technologies <62441881+FFPTech@users.noreply.github.com>
2023-01-13 11:58:05 +0100

    Correct figure 1

--- MATLAB/SP_L03_AudioFiltering.m
+++ MATLAB/SP_L03_AudioFiltering.m
@@ -11,7 +11,7 @@ figure(1);
 subplot(2, 1, 1);
 plot(x);
 title('Clean Audio Signal x(t)');
-xlabel('t');
+xlabel('t [s]');
 ylabel('x(t)');
 grid on;
 
@@ -22,51 +22,54 @@ n = length(x);
 f = (-n/2:n/2-1)*(fs/n);
 figure(1);
 subplot(2, 1, 2);
-plot(w, abs(X), 'LineWidth', 2);
+plot(f, abs(X), 'LineWidth', 2);
 xlim([0, 1000]);
+title('Clean Audio Signal X(f)');
+xlabel('f [Hz]');
+ylabel('X(f)');
 grid on;
 
 % Add white noise
-sigma = 0.1; %noise parameter
-noise = sigma*randn(size(signal)); % white noise
-noisy_signal = signal + noise; % noisy signal
-figure();
-plot(noisy_signal);
-title('Noisy Audio Signal x(t) + v(t)');
-xlabel('t');
-ylabel('x(t) + v(t)');
-grid on;
-
-% Create low-pass filter
-fc = 400; %cut-off frequency
-n = 6; % filter order
-[b, a] = butter(n, fc/(f/2));
-
-% Filter audio signal
-filtered_signal = filter(b, a, signal);
-figure();
-plot(filtered_signal);
-title('Filtered signal y(t)');
-xlabel('t');
-ylabel('y(t)');
-grid on;
-
-x = filtered_signal;
-X = fft(x);
-X = fftshift(X);
-n = length(x);
-w = (-n/2:n/2-1)*(f/n);
-figure();
-plot(w, abs(X), 'LineWidth', 2);
-xlim([0, 1000]);
-grid on;
-
-
-% Play resulting sound
-player = audioplayer(filtered_signal, f);
-%play(player); % play sound
-
-%player = audioplayer(filtered_signal, frequency);
-%play(player); % play sound
+% sigma = 0.1; %noise parameter
+% noise = sigma*randn(size(signal)); % white noise
+% noisy_signal = signal + noise; % noisy signal
+% figure();
+% plot(noisy_signal);
+% title('Noisy Audio Signal x(t) + v(t)');
+% xlabel('t');
+% ylabel('x(t) + v(t)');
+% grid on;
+% 
+% % Create low-pass filter
+% fc = 400; %cut-off frequency
+% n = 6; % filter order
+% [b, a] = butter(n, fc/(f/2));
+% 
+% % Filter audio signal
+% filtered_signal = filter(b, a, signal);
+% figure();
+% plot(filtered_signal);
+% title('Filtered signal y(t)');
+% xlabel('t');
+% ylabel('y(t)');
+% grid on;
+% 
+% x = filtered_signal;
+% X = fft(x);
+% X = fftshift(X);
+% n = length(x);
+% w = (-n/2:n/2-1)*(f/n);
+% figure();
+% plot(w, abs(X), 'LineWidth', 2);
+% xlim([0, 1000]);
+% grid on;
+% 
+% 
+% % Play resulting sound
+% player = audioplayer(filtered_signal, f);
+% %play(player); % play sound
+% 
+% %player = audioplayer(filtered_signal, frequency);
+% %play(player); % play sound
 
 
